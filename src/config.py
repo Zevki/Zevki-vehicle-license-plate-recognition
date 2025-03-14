b42a263299231edaa916ed25e12264878a16486e
Zevki <92832086+Zevki@users.noreply.github.com>
2025-03-14 14:24:44 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ import torch
 
 NUM_WORKERS = os.cpu_count()
 
-DATA_URL = "https://raw.githubusercontent.com/lngquoctrung/vehicle-license-plate-recognition/refs/heads/main/large_file_parts"
+DATA_URL = "https://raw.githubusercontent.com/zevki/vehicle-license-plate-recognition/refs/heads/main/large_file_parts"
 DATA_DIR = "data"
 RAW_DATA_DIR = os.path.join(DATA_DIR, "raw")
 PROCESSED_DATA_DIR = os.path.join(DATA_DIR, "processed")
